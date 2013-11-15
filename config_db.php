b076e887162142e0f95fb85a9caf04480d96f25b
Maxim Baikuzin <maxim@baikuzin.com>
2013-11-15 12:04:51 +0600

    Update

---
+++ config_db.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+* Settings for your database MySQL    
+*/
+interface config_db {
+	// DB #1
+	const DB_HOST_local = 'localhost';
+	const DB_USER_local = 'user';  
+	const DB_PASS_local = 'pass'; 
+	const DB_NAME_local = 'db1'; 
+
+	// DB #2
+	const DB_HOST_log = 'localhost';
+	const DB_USER_log = 'user';  
+	const DB_PASS_log = 'pass'; 
+	const DB_NAME_log = 'db2';
+}
+  
+?>
